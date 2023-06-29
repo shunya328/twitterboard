65293b51a9d17987f7e47904220165226b677294
shunya funoki <shunyafunoki@shunyanoMac-mini.local>
2023-06-29 16:02:01 +0900

    POSTメソッドの関数を別ファイルに分けました

--- pages.js
+++ pages.js
@@ -264,79 +264,79 @@ const postPage = (req, res, data) => {
   return;
 }
 
-// 投稿ページ(POST)
-const postPostPage = (req, res, currentUserID) => {
+// // 投稿ページ(POST)
+// const postPostPage = (req, res, currentUserID) => {
 
-  //マルチパートフォームデータを処理するためのヘルパー関数たちを宣言
-  function extractBoundary(contentType) {
-    console.log(req.headers['content-type']);
-    const boundaryMatch = contentType.match(/boundary=(?:"([^"]+)"|([^;]+))/i);
-    return boundaryMatch && (boundaryMatch[1] || boundaryMatch[2]);
-  }
+//   //マルチパートフォームデータを処理するためのヘルパー関数たちを宣言
+//   function extractBoundary(contentType) {
+//     console.log(req.headers['content-type']);
+//     const boundaryMatch = contentType.match(/boundary=(?:"([^"]+)"|([^;]+))/i);
+//     return boundaryMatch && (boundaryMatch[1] || boundaryMatch[2]);
+//   }
 
-  function parseFormData(body, boundary) {
-    const formData = {};
-    const parts = body.split(`--${boundary}`); //仕様で決まっていないかも「--」
+//   function parseFormData(body, boundary) {
+//     const formData = {};
+//     const parts = body.split(`--${boundary}`); //仕様で決まっていないかも「--」
 
-    // 最初と最後のパートは境界線のみのデータなので無視する
-    for (let i = 1; i < parts.length - 1; i++) {
-      const part = parts[i].trim();
+//     // 最初と最後のパートは境界線のみのデータなので無視する
+//     for (let i = 1; i < parts.length - 1; i++) {
+//       const part = parts[i].trim();
 
-      // パートのヘッダーとコンテンツを分割する
-      const [header, content] = part.split('\r\n\r\n');
-      const nameMatch = header.match(/name="([^"]+)"/);
+//       // パートのヘッダーとコンテンツを分割する
+//       const [header, content] = part.split('\r\n\r\n');
+//       const nameMatch = header.match(/name="([^"]+)"/);
 
-      if (nameMatch) {
-        const fieldName = nameMatch[1];
-        // formData[fieldName] = Buffer.from(content, 'binary').toString('utf-8');
-        formData[fieldName] = content;
-      }
-    }
-    return formData;
-  }
+//       if (nameMatch) {
+//         const fieldName = nameMatch[1];
+//         // formData[fieldName] = Buffer.from(content, 'binary').toString('utf-8');
+//         formData[fieldName] = content;
+//       }
+//     }
+//     return formData;
+//   }
 
-  header(req, res);
-  //まずはPOSTで送られたデータを受け取る
-  //dataイベントでPOSTされたデータがchunkに分けられてやってくるので、bodyに蓄積する
-  let body = [];
-  req.on('data', (chunk) => {
-    body.push(chunk);
-  }).on('end', () => {
-    const queryString = require('querystring');
-    const parseBody = queryString.parse(body);
-    body = Buffer.concat(body).toString('binary'); //Buffer.concat()メソッドで複数のBufferオブジェクト(body)を結合し新たなBufferオブジェクトを生成。
-    // body = Buffer.concat(body,'binary').toString('utf-8'); //Buffer.concat()メソッドで複数のBufferオブジェクト(body)を結合し新たなBufferオブジェクトを生成。
+//   header(req, res);
+//   //まずはPOSTで送られたデータを受け取る
+//   //dataイベントでPOSTされたデータがchunkに分けられてやってくるので、bodyに蓄積する
+//   let body = [];
+//   req.on('data', (chunk) => {
+//     body.push(chunk);
+//   }).on('end', () => {
+//     const queryString = require('querystring');
+//     const parseBody = queryString.parse(body);
+//     body = Buffer.concat(body).toString('binary'); //Buffer.concat()メソッドで複数のBufferオブジェクト(body)を結合し新たなBufferオブジェクトを生成。
+//     // body = Buffer.concat(body,'binary').toString('utf-8'); //Buffer.concat()メソッドで複数のBufferオブジェクト(body)を結合し新たなBufferオブジェクトを生成。
 
-    // フォームデータの解析
-    const contentType = req.headers['content-type'];
-    const boundary = extractBoundary(contentType); // Content-Typeヘッダからマルチパートフォームデータの境界(boudary)を抽出する
+//     // フォームデータの解析
+//     const contentType = req.headers['content-type'];
+//     const boundary = extractBoundary(contentType); // Content-Typeヘッダからマルチパートフォームデータの境界(boudary)を抽出する
 
-    if (boundary) {
-      const formData = parseFormData(body, boundary);
+//     if (boundary) {
+//       const formData = parseFormData(body, boundary);
 
-      // フォームデータの取得
-      const { kakikomi, image, reply_to } = formData;
-      const kakikomiToString = Buffer.from(kakikomi, 'binary').toString('utf-8') //ここで、バイナリデータを正しく文字列に変換(日本語に対応)
+//       // フォームデータの取得
+//       const { kakikomi, image, reply_to } = formData;
+//       const kakikomiToString = Buffer.from(kakikomi, 'binary').toString('utf-8') //ここで、バイナリデータを正しく文字列に変換(日本語に対応)
 
-      if (kakikomiToString || image) {
-        insertPost(kakikomiToString, image, reply_to, currentUserID)
-          .then((imagePath) => {
-            res.write('<h2>ツイート（文字）投稿しました</h2>\n');
-            res.write(`投稿内容: ${decodeURIComponent(kakikomiToString)}`);
-            res.write('<h2>ツイート（画像）投稿しました</h2>\n');
-            res.write(`画像パス: ${imagePath}`);
-            footer(req, res);
-          })
-          .catch((err) => {
-            console.error(err.message);
-            res.write('<h2>エラーが発生しました</h2>\n');
-            footer(req, res);
-          })
-      }
-    }
-    return;
-  });
-}
+//       if (kakikomiToString || image) {
+//         insertPost(kakikomiToString, image, reply_to, currentUserID)
+//           .then((imagePath) => {
+//             res.write('<h2>ツイート（文字）投稿しました</h2>\n');
+//             res.write(`投稿内容: ${decodeURIComponent(kakikomiToString)}`);
+//             res.write('<h2>ツイート（画像）投稿しました</h2>\n');
+//             res.write(`画像パス: ${imagePath}`);
+//             footer(req, res);
+//           })
+//           .catch((err) => {
+//             console.error(err.message);
+//             res.write('<h2>エラーが発生しました</h2>\n');
+//             footer(req, res);
+//           })
+//       }
+//     }
+//     return;
+//   });
+// }
 
 // 投稿の詳細画面(GET)
 const showPost = (req, res, postID, currentUserID) => {
@@ -500,125 +500,125 @@ const editProfilePage = (req, res) => {
   footer(req, res);
 }
 
-//（UPDATE）プロフィール編集実行！
-const updateEditProfilePage = (req, res, currentUser) => {
-  return new Promise((resolve, reject) => {
+// //（UPDATE）プロフィール編集実行！
+// const updateEditProfilePage = (req, res, currentUser) => {
+//   return new Promise((resolve, reject) => {
 
-    //マルチパートフォームデータを処理するためのヘルパー関数たちを宣言
-    function extractBoundary(contentType) {
-      console.log(req.headers['content-type']);
-      const boundaryMatch = contentType.match(/boundary=(?:"([^"]+)"|([^;]+))/i);
-      return boundaryMatch && (boundaryMatch[1] || boundaryMatch[2]);
-    }
+//     //マルチパートフォームデータを処理するためのヘルパー関数たちを宣言
+//     function extractBoundary(contentType) {
+//       console.log(req.headers['content-type']);
+//       const boundaryMatch = contentType.match(/boundary=(?:"([^"]+)"|([^;]+))/i);
+//       return boundaryMatch && (boundaryMatch[1] || boundaryMatch[2]);
+//     }
 
-    function parseFormData(body, boundary) {
-      const formData = {};
-      const parts = body.split(`--${boundary}`); //仕様で決まっていないかも「--」
+//     function parseFormData(body, boundary) {
+//       const formData = {};
+//       const parts = body.split(`--${boundary}`); //仕様で決まっていないかも「--」
 
-      // 最初と最後のパートは境界線のみのデータなので無視する
-      for (let i = 1; i < parts.length - 1; i++) {
-        const part = parts[i].trim();
+//       // 最初と最後のパートは境界線のみのデータなので無視する
+//       for (let i = 1; i < parts.length - 1; i++) {
+//         const part = parts[i].trim();
 
-        // パートのヘッダーとコンテンツを分割する
-        const [header, content] = part.split('\r\n\r\n');
-        const nameMatch = header.match(/name="([^"]+)"/);
+//         // パートのヘッダーとコンテンツを分割する
+//         const [header, content] = part.split('\r\n\r\n');
+//         const nameMatch = header.match(/name="([^"]+)"/);
 
-        if (nameMatch) {
-          const fieldName = nameMatch[1];
-          // formData[fieldName] = Buffer.from(content, 'binary').toString('utf-8');
-          formData[fieldName] = content;
-        }
-      }
-      return formData;
-    }
+//         if (nameMatch) {
+//           const fieldName = nameMatch[1];
+//           // formData[fieldName] = Buffer.from(content, 'binary').toString('utf-8');
+//           formData[fieldName] = content;
+//         }
+//       }
+//       return formData;
+//     }
 
-    header(req, res);
-    //まずはPOSTで送られたデータを受け取る
-    //dataイベントでPOSTされたデータがchunkに分けられてやってくるので、bodyに蓄積する
-    let body = [];
-    req.on('data', (chunk) => {
-      body.push(chunk);
-    }).on('end', () => {
-      // body = Buffer.concat(body).toString(); //Buffer.concat()メソッドで複数のBufferオブジェクト(body)を結合し新たなBufferオブジェクトを生成。それをtoString()メソッドで文字列に変換しさらにbodyに格納
-      //パースする。ここでは、queryString.parse()メソッドを使って、文字列などを解析し、オブジェクトとして返します。
-      const queryString = require('querystring');
-      const parseBody = queryString.parse(body);
-      body = Buffer.concat(body).toString('binary'); //Buffer.concat()メソッドで複数のBufferオブジェクト(body)を結合し新たなBufferオブジェクトを生成。
+//     header(req, res);
+//     //まずはPOSTで送られたデータを受け取る
+//     //dataイベントでPOSTされたデータがchunkに分けられてやってくるので、bodyに蓄積する
+//     let body = [];
+//     req.on('data', (chunk) => {
+//       body.push(chunk);
+//     }).on('end', () => {
+//       // body = Buffer.concat(body).toString(); //Buffer.concat()メソッドで複数のBufferオブジェクト(body)を結合し新たなBufferオブジェクトを生成。それをtoString()メソッドで文字列に変換しさらにbodyに格納
+//       //パースする。ここでは、queryString.parse()メソッドを使って、文字列などを解析し、オブジェクトとして返します。
+//       const queryString = require('querystring');
+//       const parseBody = queryString.parse(body);
+//       body = Buffer.concat(body).toString('binary'); //Buffer.concat()メソッドで複数のBufferオブジェクト(body)を結合し新たなBufferオブジェクトを生成。
 
-      // フォームデータの解析
-      const contentType = req.headers['content-type'];
-      const boundary = extractBoundary(contentType); // Content-Typeヘッダからマルチパートフォームデータの境界(boudary)を抽出する
+//       // フォームデータの解析
+//       const contentType = req.headers['content-type'];
+//       const boundary = extractBoundary(contentType); // Content-Typeヘッダからマルチパートフォームデータの境界(boudary)を抽出する
 
-      if (boundary) {
-        const formData = parseFormData(body, boundary);
+//       if (boundary) {
+//         const formData = parseFormData(body, boundary);
 
-        // フォームデータの取得
-        const { user_name, user_email, user_password, user_profile, user_image } = formData;
-        console.log('user_name:', user_name);
-        const userNameToString = (user_name ? Buffer.from(user_name, 'binary').toString('utf-8') : null); //ここで、バイナリデータを正しく文字列に変換(日本語に対応)
-        const userEmailToString = (user_email ? Buffer.from(user_email, 'binary').toString('utf-8') : null);
-        const userPasswordToString = (user_password ? Buffer.from(user_password, 'binary').toString('utf-8') : null);
-        const userProfileToString = (user_profile ? Buffer.from(user_profile, 'binary').toString('utf-8') : null);
+//         // フォームデータの取得
+//         const { user_name, user_email, user_password, user_profile, user_image } = formData;
+//         console.log('user_name:', user_name);
+//         const userNameToString = (user_name ? Buffer.from(user_name, 'binary').toString('utf-8') : null); //ここで、バイナリデータを正しく文字列に変換(日本語に対応)
+//         const userEmailToString = (user_email ? Buffer.from(user_email, 'binary').toString('utf-8') : null);
+//         const userPasswordToString = (user_password ? Buffer.from(user_password, 'binary').toString('utf-8') : null);
+//         const userProfileToString = (user_profile ? Buffer.from(user_profile, 'binary').toString('utf-8') : null);
 
-        if (!user_name && !user_email && !user_password && !user_profile && !user_image) {
-          res.write('特になにも更新されませんでした');
-          resolve(null); //特になにも入力されなかったらnullをresolve()の引数として渡す
-          footer(req, res);
-          return;
-        }
+//         if (!user_name && !user_email && !user_password && !user_profile && !user_image) {
+//           res.write('特になにも更新されませんでした');
+//           resolve(null); //特になにも入力されなかったらnullをresolve()の引数として渡す
+//           footer(req, res);
+//           return;
+//         }
 
-        // ユーザ名のバリデーション
-        const userNameRegex = /^[a-zA-Z0-9]+$/; // 半角英数字のみを許可する正規表現
-        if (!userNameRegex.test(userNameToString) && userNameToString) {
-          // ユーザ名が正規表現にマッチしない場合
-          res.write('<h2>ユーザ情報の更新に失敗しました</h2>');
-          res.write('<h5>ユーザ名は半角英数字のみを入力してください</h5>');
-          footer(req, res);
-          return;
-        }
+//         // ユーザ名のバリデーション
+//         const userNameRegex = /^[a-zA-Z0-9]+$/; // 半角英数字のみを許可する正規表現
+//         if (!userNameRegex.test(userNameToString) && userNameToString) {
+//           // ユーザ名が正規表現にマッチしない場合
+//           res.write('<h2>ユーザ情報の更新に失敗しました</h2>');
+//           res.write('<h5>ユーザ名は半角英数字のみを入力してください</h5>');
+//           footer(req, res);
+//           return;
+//         }
 
-        // メールアドレスのバリデーション
-        const emailRegex = /^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/; // メールアドレスの正規表現
-        if (!emailRegex.test(userEmailToString) && userEmailToString) {
-          // メールアドレスが正規表現にマッチしない場合
-          res.write('<h2>ユーザ情報の更新に失敗しました</h2>');
-          res.write('<h5>正しい形式のメールアドレスを入力してください</h5>');
-          footer(req, res);
-          return;
-        }
+//         // メールアドレスのバリデーション
+//         const emailRegex = /^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/; // メールアドレスの正規表現
+//         if (!emailRegex.test(userEmailToString) && userEmailToString) {
+//           // メールアドレスが正規表現にマッチしない場合
+//           res.write('<h2>ユーザ情報の更新に失敗しました</h2>');
+//           res.write('<h5>正しい形式のメールアドレスを入力してください</h5>');
+//           footer(req, res);
+//           return;
+//         }
 
-        // 現在ログインしているユーザの情報をアップデート
-        updateUser(currentUser.userID, userNameToString, userEmailToString, userPasswordToString, userProfileToString, user_image, (err) => {
-          if (err) {
-            console.log('updateUserは回っているみたいinERROR')
-            console.error(err.message);
-            res.write('<h2>プロフィールの更新に失敗しました</h2><br>');
-            res.write(`<h4>${err.message}</h4>`);
-            reject(err); // エラー時にreject
-            footer(req, res);
-            return;
-          }
+//         // 現在ログインしているユーザの情報をアップデート
+//         updateUser(currentUser.userID, userNameToString, userEmailToString, userPasswordToString, userProfileToString, user_image, (err) => {
+//           if (err) {
+//             console.log('updateUserは回っているみたいinERROR')
+//             console.error(err.message);
+//             res.write('<h2>プロフィールの更新に失敗しました</h2><br>');
+//             res.write(`<h4>${err.message}</h4>`);
+//             reject(err); // エラー時にreject
+//             footer(req, res);
+//             return;
+//           }
 
-          console.log('updateUserは回っているみたい')
-          const newProfile = {
-            userID: currentUser.userID,
-            name: (userNameToString ? userNameToString : currentUser.name),
-            email: (userEmailToString ? userEmailToString : currentUser.email),
-            profile: (userProfileToString ? userProfileToString : currentUser.profile)
-          };
+//           console.log('updateUserは回っているみたい')
+//           const newProfile = {
+//             userID: currentUser.userID,
+//             name: (userNameToString ? userNameToString : currentUser.name),
+//             email: (userEmailToString ? userEmailToString : currentUser.email),
+//             profile: (userProfileToString ? userProfileToString : currentUser.profile)
+//           };
 
-          console.log(`updateEditProfilePage()の中のnewProfile = ${JSON.stringify(newProfile)}`);
-          res.write('<h2>プロフィールは更新されました</h2>');
-          // footer(req, res);
+//           console.log(`updateEditProfilePage()の中のnewProfile = ${JSON.stringify(newProfile)}`);
+//           res.write('<h2>プロフィールは更新されました</h2>');
+//           // footer(req, res);
 
-          // サーバ側のセッション情報を返り値に
-          resolve(newProfile); // resolveの引数に渡す
-          footer(req, res);
-        });
-      }
-    })
-  });
-}
+//           // サーバ側のセッション情報を返り値に
+//           resolve(newProfile); // resolveの引数に渡す
+//           footer(req, res);
+//         });
+//       }
+//     })
+//   });
+// }
 
 // フォロー一覧ページ
 const followingUserPage = (req, res, currentUserID) => {
@@ -782,17 +782,17 @@ const readImageFile = (req, res) => {
   });
 }
 
-// （POST）ユーザを論理削除する関数
-const postWithdrawalUser = (req, res, sessions, sessionID) => {
-  withdrawalUser(sessions[sessionID].userID, (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(`userID=${sessions[sessionID].userID}のユーザが削除されました`);
-    postLogout(req, res, sessions, sessionID);
-  })
-}
+// // （POST）ユーザを論理削除する関数
+// const postWithdrawalUser = (req, res, sessions, sessionID) => {
+//   withdrawalUser(sessions[sessionID].userID, (err) => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//     console.log(`userID=${sessions[sessionID].userID}のユーザが削除されました`);
+//     postLogout(req, res, sessions, sessionID);
+//   })
+// }
 
 // その他のページ(404 Not Found)
 const notFoundPage = (req, res) => {
@@ -810,16 +810,13 @@ module.exports = {
   userIndexPage,
   showUserPagePagenation,
   postPage,
-  postPostPage,
   showPost,
   myPage,
   editProfilePage,
-  updateEditProfilePage,
   followingUserPage,
   followerUserPage,
   searchPage,
   searchUserResultPage,
   readImageFile,
-  postWithdrawalUser,
   notFoundPage
 }
