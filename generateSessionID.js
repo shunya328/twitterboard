97447c6b3789039b565c1e930f54f1879bbb7c04
shunya funoki <shunyafunoki@shunyanoMac-mini.local>
2023-07-06 10:32:23 +0900

    セッションID作成関数を、セキュアな乱数生成ロジックに修正

--- generateSessionID.js
+++ generateSessionID.js
@@ -1,12 +1,27 @@
+const crypto = require("crypto");
+
 const generateSessionID = () => {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let sessionID = '';
-  for (let i = 0; i < 32; i++) {
-    sessionID += characters.charAt(Math.floor(Math.random() * characters.length));
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const length = 32;
+  let sessionID = "";
+
+  // 乱数生成用のバッファを作成
+  const randomBytes = crypto.randomBytes(length);
+
+  //バッファからランダムなインデックスを取得し、文字列を生成
+  // for (let i = 0; i < length; i++) {
+  //   sessionID += characters.charAt(
+  //     Math.floor(Math.random() * characters.length)
+  //   );
+  // }
+  for(let i=0; i < length; i++){
+    const randomIndex = randomBytes[i] % characters.length;
+    sessionID += characters.charAt(randomIndex);
   }
   return sessionID;
-}
+};
 
 module.exports = {
-  generateSessionID
-}
+  generateSessionID,
+};
